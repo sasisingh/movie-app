beb8a8124bf8733c843a8fe9da8509300846cb45
sasisingh <sasisingh6387@gmail.com>
2023-06-20 17:55:56 +0530

    initial commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,19 +3,19 @@ import React from 'react'
 function Card({ item }) {
 	return (
 		<div className='row my-4 bg-white'>
-			<div className='col-sm-5'>
-				<img src={`https://image.tmdb.org/t/p/original${item.poster_path}`} alt="poster" height="230px" width="300px" />
+			<div className='col-lg-5 col-md-12'>
+				<img src={`https://image.tmdb.org/t/p/original${item.poster_path}`} alt="poster"/>
 			</div>
-			<div className='col-sm-6 py-5'>
-				<div className='card-body'>
-					<b style={{ fontSize: "18px" }}>{item.original_title}</b>
-					<p>{item.overview.length > 100 ? item.overview.substr(0, 100) + "..." : item.overview}</p>
-					<p><span className='ms-4'>Release Date</span> <span className='ms-4'> {item.release_date} </span> <span className='ms-4'> {item.vote_count} Votes</span></p>
+			<div className='col-lg-7 col-md-12'>
+				<div className='card-body my-5'>
+					<b className='title'>{item.original_title}</b>
+					<p className='overview'>{item.overview.length > 100 ? item.overview.substr(0, 100) + "..." : item.overview}</p>
+					<p clasName="realease-date"> Release Date <span className='ms-4'> {item.release_date} </span> <span className='ms-4'> {item.vote_count} Votes</span></p>
 				</div>
 			</div>
-			<div className='col-sm-1 bg-light'></div>
+		 
 		</div>
 	)
 }
 
-export default Card
+export default Card ;
