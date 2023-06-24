a4b9c44b923dc9d59fc6ff7aa8daee1e9164e30b
sasisingh <sasisingh6387@gmail.com>
2023-06-24 18:57:13 +0530

    initial commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ function Card({ item }) {
 	return (
 		<div className='row my-4 bg-white'>
 			<div className='col-lg-5 col-md-12'>
-				<img src={`https://image.tmdb.org/t/p/original${item.poster_path}`} alt="poster"/>
+				<img src={`https://image.tmdb.org/t/p/original${item.poster_path}`} alt="poster" />
 			</div>
 			<div className='col-lg-7 col-md-12'>
 				<div className='card-body my-5'>
@@ -13,9 +13,8 @@ function Card({ item }) {
 					<p clasName="realease-date"> Release Date <span className='ms-4'> {item.release_date} </span> <span className='ms-4'> {item.vote_count} Votes</span></p>
 				</div>
 			</div>
-		 
 		</div>
 	)
 }
 
-export default Card ;
+export default Card;
